6e6536b9ee6e04c0cf707fdf670b60aa91b35d0f
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2019-07-05 13:21:58 -0300

    Add files via upload

---
+++ ex1atv2/ap02_01b.php
@@ -0,0 +1,17 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Atividade Prática 02 - Seletor de Carros</title>
+</head>
+<body>
+    <h2>Sucesso!</h2>
+    <?php
+    $tipo=$_POST['selTipo'];
+    $cor = $_POST['txtcor'];
+
+    echo "Seu $tipo de $cor está pronto. Dirija com cuidado!";
+    ?>
+    
+</body>
+</html>
